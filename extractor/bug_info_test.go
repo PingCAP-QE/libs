a64df27155d7b1cb88d9f91c8e1867976339801a
lob <pengyu@pingcap.com>
2020-11-06 11:50:55 +0800

    update representation of string in extractor test

--- extractor/bug_info_test.go
+++ extractor/bug_info_test.go
@@ -45,7 +45,6 @@ func logErrMap(t *testing.T, m map[string][]error) {
 }
 
 func TestParseCommentBodyFromString(t *testing.T) {
-	var tmp = "## Please edit this comment to complete the following information\r\n\r\n### Not a bug\r\n\r\n1. Remove the 'type/bug' label\r\n2. Add notes to indicate why it is not a bug\r\n\r\n### Duplicate bug\r\n\r\n1. Add the 'type/duplicate' label\r\n2. Add the link to the original bug\r\n\r\n### Bug\r\n\r\nNote: Make Sure that 'component', and 'severity' labels are added\r\nExample for how to fill out the template: https://github.com/pingcap/tidb/issues/20100\r\n\r\n#### 1. Root Cause Analysis (RCA) (optional) \r\n<!-- Write down the reason why this bug occurs -->\r\n\r\n#### 2. Symptom (optional)\r\n\r\n<!-- What will the user see when this bug occurs. The error message may be in the terminal, log or monitoring -->\r\n\r\n#### 3. All Trigger Conditions (optional)\r\n\r\n<!-- All the user scenarios that may trigger this bug -->\r\n\r\n#### 4. Workaround (optional)\r\n\r\n#### 5. Affected versions\r\n\r\n<!--\r\nIn the format of [start_version:end_version], multiple version ranges are\r\naccepted. If the bug only affects the unreleased version, please input:\r\n\"unreleased\". For example:\r\n\r\nNotes:\r\n  1. Do not use any white spaces in '[]'.\r\n  2. The range in '[]' is a closed interval\r\n  3. The version format is `v$Major.$Minor.$Patch`, the $Majoy and $Minor\r\n     number in a version range should be the same. [v3.0.1:v3.1.2] is\r\n     invalid because the $Minor number of the version range is different.\r\n\r\nExample 1: [v3.0.1:v3.0.5], [v4.0.1:v4.0.5]\r\nExample 2: unreleased\r\n-->\r\n[v3.0.0:v3.0.19]\r\n#### 6. Fixed versions\r\nv3.0.20\r\n<!--\r\nThe first released version that contains this fix in each minor version. If the bug's affected version has been released, the fixed version should be a detailed version number; If the bug doesn't affect any released version, the fixed version can be \"master\";  \r\n\r\nExample 1: v3.0.13, v4.0.5\r\nExample 2: master\r\n-->"
 	info, errMaps := ParseCommentBody(tmp)
 	logStuctFields(t, *info)
 	logErrMap(t, errMaps)
@@ -80,3 +79,60 @@ func TestParseCommentBodyFromCommentId(t *testing.T) {
 	logStuctFields(t, *infos)
 	logErrMap(t, errs)
 }
+
+var tmp = `## Please edit this comment to complete the following information
+        
+### Not a bug
+
+1. Remove the 'type/bug' label
+2. Add notes to indicate why it is not a bug
+
+### Duplicate bug
+
+1. Add the 'type/duplicate' label
+2. Add the link to the original bug
+
+### Bug
+
+Note: Make Sure that 'component', and 'severity' labels are added
+Example for how to fill out the template: https://github.com/pingcap/tidb/issues/20100
+
+#### 1. Root Cause Analysis (RCA) (optional) 
+<!-- Write down the reason why this bug occurs -->
+
+#### 2. Symptom (optional)
+
+<!-- What will the user see when this bug occurs. The error message may be in the terminal, log or monitoring -->
+
+#### 3. All Trigger Conditions (optional)
+
+<!-- All the user scenarios that may trigger this bug -->
+
+#### 4. Workaround (optional)
+
+#### 5. Affected versions
+
+<!--
+In the format of [start_version:end_version], multiple version ranges are
+accepted. If the bug only affects the unreleased version, please input:
+"unreleased". For example:
+
+Notes:
+  1. Do not use any white spaces in '[]'.
+  2. The range in '[]' is a closed interval
+  3. The version format is ` + "`v$Major.$Minor.$Patch`" + `, the $Majoy and $Minor
+	 number in a version range should be the same. [v3.0.1:v3.1.2] is
+	 invalid because the $Minor number of the version range is different.
+
+Example 1: [v3.0.1:v3.0.5], [v4.0.1:v4.0.5]
+Example 2: unreleased
+-->
+[v3.0.0:v3.0.19]
+#### 6. Fixed versions
+v3.0.20
+<!--
+The first released version that contains this fix in each minor version. If the bug's affected version has been released, the fixed version should be a detailed version number; If the bug doesn't affect any released version, the fixed version can be "master";  
+
+Example 1: v3.0.13, v4.0.5
+Example 2: master
+-->`
