90d355153d65a227af86d889bc66ffce8b7c8578
xutianshu <1577840123@qq.com>
2020-12-07 18:28:33 +0800

    feat(coverage): remove useless token

--- coverage/coverage.go
+++ coverage/coverage.go
@@ -10,27 +10,27 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // ProcessCoverage gets the coverage of {owner}/{repo} after each commit in the past year through codecov's API, saves into mysql
-func ProcessCoverage(owner, repo, token string) {
+func ProcessCoverage(owner, repo string) error {
 	log.Printf("Processing %s\n", owner+"/"+repo)
 	client := http.Client{}
 	req, err := http.NewRequest("GET", "https://codecov.io/api/gh/"+owner+"/"+repo+"/branch/master/graphs/commits.json?method=min&agg=day&time=365d&inc=totals&order=asc", strings.NewReader(""))
 	if err != nil {
-		panic(err)
+		return err
 	}
-
-	req.Header.Set("Authorization", "token "+token)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	var message interface{}
@@ -39,11 +39,11 @@ func ProcessCoverage(owner, repo, token string) {
 
 	db, err := sql.Open("mysql", os.Getenv("GITHUB_DSN"))
 	if err != nil {
-		panic(err)
+		return err
 	}
 	tx, err := db.Begin()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	commits := message.(map[string]interface{})["commits"]
@@ -52,19 +52,21 @@ func ProcessCoverage(owner, repo, token string) {
 		totals := commit.(map[string]interface{})["totals"]
 		coverage, err := strconv.ParseFloat(totals.(map[string]interface{})["c"].(string), 64)
 		if err != nil {
-			panic(err)
+			return err
 		}
 		t, err := time.Parse("2006-01-02 15:04:05", timestamp.(string))
 		if err != nil {
-			panic(err)
+			return err
 		}
 		_, err = tx.Exec("INSERT INTO coverage_timeline(repo, time, coverage) VALUES(?, ?, ?)", repo, t, coverage)
 		if err != nil {
-			panic(err)
+			return err
 		}
 	}
 
 	tx.Commit()
 
 	log.Printf("Finish %s\n", owner+"/"+repo)
+
+	return nil
 }
