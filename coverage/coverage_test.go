0b9b73d53e8d52e1a9f99c3a2090f05a84d74d8e
xutianshu <1577840123@qq.com>
2020-12-10 17:17:36 +0800

    feat(coverage): handle error

--- coverage/coverage_test.go
+++ coverage/coverage_test.go
@@ -1,11 +1,19 @@
 package coverage
 
 import (
+	"database/sql"
+	"os"
 	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestProcessCoverage(t *testing.T) {
-	err := ProcessCoverage("pingcap", "tidb")
+	db, err := sql.Open("mysql", os.Getenv("GITHUB_DSN"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ProcessCoverage(db, "pingcap", "tidb-lightning")
 	if err != nil {
 		t.Fatal(err)
 	}
